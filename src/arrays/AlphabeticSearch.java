46cf157874d232bfaafeadcdd31cf075abfbe37d
xufx <1454130786@qq.com>
2017-07-03 11:30:03 +0800

    only untrack .idea/workspace.xml

--- src/arrays/AlphabeticSearch.java
+++ src/arrays/AlphabeticSearch.java
@@ -13,7 +13,6 @@ public class AlphabeticSearch
         String[] sa= Generated.array(new String[30],new RandomGenerator.String(5));
         Arrays.sort(sa, String.CASE_INSENSITIVE_ORDER);
         System.out.println(Arrays.toString(sa));
-
         int index=Arrays.binarySearch(sa,sa[10],String.CASE_INSENSITIVE_ORDER);
         System.out.println("index:"+index+"\n"+sa[index]);
 
